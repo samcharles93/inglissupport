1791c7b5b420a9803a78193034535746433adad4
Sam Catlow <sam@catlow.tech>
2020-12-19 12:10:25 +1100

    import auth to main.js to get user before mounting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { auth } from './firebase/config'
 import store from './store'
 
 // Styles 
@@ -8,7 +9,8 @@ import './assets/css/theme.css'
 import './assets/css/utilities.css'
 import './assets/css/components.css'
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app')
+let app
+
+auth.onAuthStateChanged(() => {
+    app = createApp(App).use(store).use(router).mount('#app')
+})
