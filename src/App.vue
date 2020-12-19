f0e840650c111bb531c4524004abab16534166e5
Sam Catlow <sam@catlow.tech>
2020-12-19 12:04:42 +1100

    add page transition for smoothness

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
   <Navbar />
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition
+      mode="out-in"
+      enter-active-class="fadeIn"
+      leave-active-class="fadeOut"
+    >
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <Footer />
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
-import Footer from '@/components/Footer.vue'
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
-  export default {
-    components: { Navbar, Footer }
-  }
+export default {
+  components: { Navbar, Footer }
+};
 </script>
 
-<style></style>
+<style>
+.fadeIn {
+  animation-duration: 0.5s;
+  animation-fill-mode: both;
+  animation-name: fadeIn;
+}
+.fadeOut {
+  animation-duration: 0.5s;
+  animation-fill-mode: both;
+  animation-name: fadeOut;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+}
+</style>
