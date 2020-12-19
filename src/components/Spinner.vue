6dc41ca12d68c3a3b829c68a75e47e4c1e32fc27
Sam Catlow <sam@catlow.tech>
2020-12-19 12:12:41 +1100

    create loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,21 @@
+<template>
+      <div class="spin"></div>
+</template>
+
+<style>
+.spin {
+  display: block;
+  width: 30px;
+  height: 30px;
+  margin: auto;
+  border: 4px solid transparent;
+  border-radius: 100%;
+  border-top-color: var(--accent);
+  animation: spin 1.3s linear infinite;
+}
+@keyframes spin {
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+</style>
