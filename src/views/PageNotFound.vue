8dae31ef0172f53af22147909ea1c9884e09088b
Sam Catlow <sam@catlow.tech>
2021-01-03 01:15:04 +1100

    fix 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,14 +1,26 @@
 <template>
-  404
-  <router-link :to="{ name: 'Home' }">Home</router-link>
+  <div class="container">
+    <h1 class="text-center my-3">404</h1>
+    <p class="text-center my-3">
+      Sorry, the page you are trying to access is not available.
+      Please try again later.
+    </p>
+    <div>
+      <a class="btn-alt" href="/">Go Home</a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  height: 60vh;
+}
+.container div {
+  display: flex;
+  justify-content: center;
+}
 </style>
