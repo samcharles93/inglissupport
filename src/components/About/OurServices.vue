8cf6fdf74a17f38499a0bb14fcf89a67d7027f1b
Sam Catlow <sam@catlow.tech>
2021-01-03 14:46:21 +1100

    create services and accordion component

--- src/components/About/OurServices.vue
+++ src/components/About/OurServices.vue
@@ -5,55 +5,78 @@
 
   <div class="services">
     <h4 class="">Services we provide</h4>
-    <ul>
-      <li>
-        <h4 @click="accodionClicked(0)">Community Support</h4>
-        <p>stuff</p>
-      </li>
-      <li>
-        <h4 @click="accodionClicked(1)">Community Support</h4>
-        <p>stuff</p>
-      </li>
-      <li>
-        <h4 @click="accodionClicked(2)">Community Support</h4>
-        <p>stuff</p>
-
-      </li>
-      <li>
-        <h4 @click="accodionClicked(3)">Community Support</h4>
-        <p>stuff</p>
-      </li>
-    </ul>
+    <Accordion title="Community Support">
+      <p>
+        At Inglis Support, we believe that getting involved with community
+        activities, events and outings help our clients foster a strong
+        community presence, help build practical skills, develop strong
+        interpersonal relationships and most importantly, have fun.
+      </p>
+    </Accordion>
+    <Accordion title="Support Coordination">
+      <p>
+        Support coordination is in place to help you get the most of your
+        funding package and get your plan on track.
+      </p>
+      <p>
+        Services that can help you achieve your plan and goals include but are
+        not limited to
+      </p>
+      <ol>
+        <li>
+          Helping get the most of your funding and services by coordinating
+          service providers and obtaining quotes in line with your plan
+        </li>
+        <li>Managing your service providers and support team</li>
+        <li>
+          Connecting you to work, training providers and/or community activities
+        </li>
+        <li>Finding the right services to suit your unique needs</li>
+      </ol>
+    </Accordion>
+    <Accordion title="Short Term Accomodation">
+      <p>
+        Our respite services are here to help you, to take care of your loved
+        ones when you need a break, need time for activities or just to relax,
+        deal with stresses of everyday life and look after yourself.
+      </p>
+    </Accordion>
+    <Accordion title="Tenancy Services">
+      <p>
+        With our tenancy support services we can help you find the right
+        location and accommodation, we can represent you in meetings with real
+        estate agencies, help you complete your application and help arrange
+        inspections, moving, purchasing furniture and getting utilities
+        connected.
+      </p>
+    </Accordion>
   </div>
 </template>
 
 <script>
-export default {
-  setup() {
-    const accodionClicked = index => {
-      const tInfoEl = document.querySelectorAll('[id="' + index + '"]')[0];
-      if (tInfoEl.classList.contains("block")) {
-        tInfoEl.classList.remove("block");
-        tInfoEl.classList.add("hidden");
-      } else {
-        tInfoEl.classList.remove("hidden");
-        tInfoEl.classList.add("block");
-      }
-    };
+import Accordion from "@/components/Accordion.vue";
 
-    return { accodionClicked };
-  }
+export default {
+  components: { Accordion }
 };
 </script>
 
 <style scoped>
 h2 {
   margin-bottom: 6px;
+  text-align: center;
 }
-
-ul {
-  list-style-type: square;
-  margin-left: 2rem;
+.services {
   font-size: medium;
+  padding: 0.5rem 0;
+}
+.services h4 {
+  margin-bottom: 0.5rem;
+}
+@media (min-width: 720px) {
+  .services {
+    margin: 0 auto;
+    width: 90%;
+  }
 }
 </style>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="service">
+    <div @click.prevent="active = !active" class="heading">
+      <h4>{{ title }}</h4>
+      <span class="down-Arrow" v-show="!active">&#9660;</span>
+      <span class="up-Arrow" v-show="active">&#9650;</span>
+    </div>
+    <div class="content" v-show="active"><slot /></div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  props: [ "title" ],
+  setup(props, context) {
+
+    const active = ref(false);
+
+    return { active };
+  }
+};
+</script>
+
+<style>
+.service .heading {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  background-color: var(--accent);
+  cursor: pointer;
+  border-bottom: 1px solid var(--primary);
+}
+.service .heading h4 {
+  color: white;
+  font-size: 1rem;
+  padding: 0.5rem;
+}
+.service .heading span {
+  color: white;
+  font-size: 1rem;
+  padding: 0.5rem;
+}
+.service .content {
+  background-color: var(--primary);
+  padding: 1rem;
+}
+.service .content p {
+  text-align: justify;
+  
+}
+.service .content a {
+
+}
+.service .content ol {
+  list-style-type: square;
+  padding: 1rem;
+}
+
+ol {
+}
+</style>
